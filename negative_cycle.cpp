2cc202417ddb1310097a1d87b7192c6b19cd310b
Naeem-Shuvo <2305146@ugrad.cse.buet.ac.bd>
2025-12-13 23:45:04 +0600

    done, taken dist[i]=0 so that every node can be potential starting point,so that disconnected graph portions from starting node can be relaxed at some point ,MODIFIED BELLMAN_FORD

--- negative_cycle.cpp
+++ negative_cycle.cpp
@@ -2,7 +2,8 @@
 using namespace std;
 void detectNegCycle(vector<vector<int>> &edge, int nodes)
 {
-    vector<int> dist(nodes + 1, INT_MAX); // nodes+1 --> 1-indexing
+    vector<int> dist(nodes + 1, 0); // nodes+1 --> 1-indexing
+                                    //sob node ke possible starting point dhora
     vector<int> parent(nodes + 1, -1);
     dist[1] = 0;
     dist[0]=-1; //use hocche na
@@ -20,12 +21,41 @@ void detectNegCycle(vector<vector<int>> &edge, int nodes)
             }
         }
     }
-    for (int i : dist)
-    {
-        cout << i<< " ";
+    // for (int i : dist)
+    // {
+    //     cout << i<< " ";
+    // }
+    // cout << endl;
+    int negaNode=-1;
+    for (auto it : edge)
+        {
+            int a = it[0] ; // dist array 1 indexed
+            int b = it[1] ;
+            int c = it[2];
+            if (dist[a] != INT_MAX && dist[a] + c < dist[b])
+            {
+                negaNode=a;
+                break;
+            }
+        }
+        if(negaNode==-1){
+            cout<<-1; //no nega cycle
+            return;
+        }
+        int it=negaNode;
+        vector<int> path;
+    while(parent[it]!=negaNode){
+        // cout<<it<<" ";
+        path.push_back(it);
+        it=parent[it];
     }
-    cout << endl;
-
+    path.push_back(it);  ///condition check kore last e ekta print na korei beroy jay tai
+    reverse(path.begin(),path.end());
+    for(int i:path) cout<<i<<" ";
+    //    for(int i=0;i<4;i++){
+    //     cout<<it<<" ";
+    //     it=parent[it];
+    //    }
 }
 int main()
 {
